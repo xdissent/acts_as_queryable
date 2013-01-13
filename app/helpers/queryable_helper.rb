ba5675226fc04603c33357e44d58a79df6a66271
Greg Thornton <xdissent@me.com>
2013-01-13 04:30:41 -0600

    Make query help use all keys from session.

--- app/helpers/queryable_helper.rb
+++ app/helpers/queryable_helper.rb
@@ -3,7 +3,7 @@
 module QueryableHelper
 
   def operators_for_select(filter_type, query_class=nil)
-    query_class ||= @query && @query.class || self.class.read_inheritable_attribute('query_class')
+    query_class ||= @query_class || (@query && @query.class) || self.class.read_inheritable_attribute('query_class')
     return [] unless query_class
     query_class.operators_by_filter_type[filter_type].collect {|o| [l(query_class.operators[o]), o]}
   end
@@ -34,20 +34,20 @@ module QueryableHelper
   end
 
   def query_session_key
-    query_class = self.class.read_inheritable_attribute('query_class')
-    "query_#{query_class.name.underscore.gsub '/', '_'}"
+    "query_#{@query_class.name.underscore.gsub '/', '_'}"
   end
 
   def find_query_object
-    query_class = self.class.read_inheritable_attribute('query_class')
-    return unless query_class
+    @query_class ||= self.class.read_inheritable_attribute('query_class')
+    return unless @query_class
 
     if !params[:query_id].blank?
-      @query = query_class.find(params[:query_id])
+      @query = @query_class.find_by_id(params[:query_id])
+      return unless @query
       session[query_session_key] = {:id => @query.id}
     else
       if params[:set_filter] || session[query_session_key].nil?
-        @query = query_class.new :name => "_"
+        @query = @query_class.new :name => "_"
         if params[:fields] || params[:f]
           @query.filters = {}
           @query.add_filters(params[:fields] || params[:f], params[:operators] || params[:op], params[:values] || params[:v])
@@ -58,12 +58,11 @@ module QueryableHelper
         end
         @query.group_by = params[:group_by]
         @query.column_names = params[:c] || (params[:query] && params[:query][:column_names])
-        session[query_session_key] = {:project_id => @query.project_id, :filters => @query.filters, :group_by => @query.group_by, :column_names => @query.column_names, :display_subprojects => @query.display_subprojects}
+        session[query_session_key] = {:filters => @query.filters, :group_by => @query.group_by, :column_names => @query.column_names}
       else
-        @query = query_class.find_by_id(session[query_session_key][:id]) if session[query_session_key][:id]
-        @query ||= query_class.new :name => "_", :filters => session[query_session_key][:filters], :group_by => session[query_session_key][:group_by], :column_names => session[query_session_key][:column_names]
+        @query = @query_class.find_by_id(session[query_session_key][:id]) if session[query_session_key][:id]
+        @query ||= @query_class.new(session[query_session_key].merge :id => nil, :name => "_")
       end
     end
-  rescue ActiveRecord::RecordNotFound
   end
 end
